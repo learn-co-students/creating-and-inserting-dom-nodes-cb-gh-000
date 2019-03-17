eea8f16ea65df5aee469795c0130133cb3f6ea9f
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-17 04:56:18 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerHTML = "Hello, Aamir Kalimi!";
+element.style.backgroundColor = '#324ff2';
+element.style.textAlign = 'center';
+element.style.color = 'white';
+document.body.appendChild(element);

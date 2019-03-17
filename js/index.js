21161ecb1de90b61f06f492e4a9d4f7cbc3fc314
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-17 04:57:38 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ element.style.backgroundColor = '#324ff2';
 element.style.textAlign = 'center';
 element.style.color = 'white';
 document.body.appendChild(element);
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+ul.style.textAlign = 'left';
